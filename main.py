ad153a3fba066dcec2451397dd94ce4ce24bc455
fcode101 <techwbro@gmail.com>
2025-01-12 12:21:17 +0530

    main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 
 app = FastAPI()
 
-# Load Thirukkural once at startup
+# Declare Global 
 thirukkural = []
 english = []
 
@@ -11,15 +11,22 @@ english = []
 def load_thirukkural():
     global thirukkural
     global english
+              # Read Tamil version of Thirukkural
     with open('thirukkural.txt', 'r', encoding='utf8') as file:
         thirukkural = [line.strip() for line in file if line.strip()] 
+        
+                # Read English version of Thirukkural
     with open('English_thirukkural.txt', 'r', encoding='utf8') as file:
         english = [line.strip() for line in file if line.strip()]     
 
 @app.get("/")
 def read_thirukkural():
-    random_index = random.randint(0, len(thirukkural) - 1)  # Get a random index
+    
+    #Get random Thirukkural
+    random_index = random.randint(0, len(thirukkural) - 1)  
     tamil_version = thirukkural[random_index]
+    
+     # Get the corresponding English version or provide fallback if out of bounds
     english_version = english[random_index] if random_index < len(english) else "No translation available"
 
     return {
